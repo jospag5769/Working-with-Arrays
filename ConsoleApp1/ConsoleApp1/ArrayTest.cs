10e146fc87042cb09abdc9f4331e603eeeb969e3
jospag5769 <39542495+jospag5769@users.noreply.github.com>
2018-06-11 18:56:36 -0400

    1st Commit

---
+++ ConsoleApp1/ConsoleApp1/ArrayTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class ArrayTest
+    {
+        static void Main(string[] args)
+        {
+            //input from user
+            //counters to keep track of how many numbers are above or below the input
+            //example array to test against
+            int input; 
+            int above = 0, below = 0; 
+            int[] arrayToTestAgainst = new int[5] {1, 5, 2, 1, 10 }; 
+
+            //getting input and converting the string to an integer
+            Console.Write("Enter a number: ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            //This for loop is in charge of counting how many numbers are greater than the input
+            for(int i = 0; i<arrayToTestAgainst.Length; i++)
+            {
+                if (input > arrayToTestAgainst[i])
+                    below++;
+                else if (input < arrayToTestAgainst[i])
+                    above++;
+            }
+            Console.WriteLine("In the array there are {0} numbers above and {1} numbers below the input", above, below);
+            Console.WriteLine();
+        }
+    }
+}
